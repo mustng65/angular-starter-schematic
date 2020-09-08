ec1b1030dc1881583e56a183b201239462a9dfd8
Dave <mustng65@hotmail.com>
2020-09-07 22:13:59 -0700

    added material framework setup

--- src/angular-starter/index.ts
+++ src/angular-starter/index.ts
@@ -1,10 +1,13 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, schematic, chain } from '@angular-devkit/schematics';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function angularStarterSchematic(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    return chain([
+      schematic('material', _options),
+    ]);
     return tree;
   };
 }
---
+++ src/material-setup/index.ts
@@ -0,0 +1,74 @@
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  externalSchematic,
+  chain,
+  noop,
+} from '@angular-devkit/schematics';
+import { addImport, hasDependency, addExport } from '../util/util';
+
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+export function AddMaterial(_options: any): Rule {
+  const matModuleList = [
+    { module: 'MatToolbarModule', path: '@angular/material/toolbar' },
+    { module: 'MatButtonModule', path: '@angular/material/button' },
+    { module: 'MatInputModule', path: '@angular/material/input' },
+    { module: 'MatIconModule', path: '@angular/material/icon' },
+  ];
+  const matFileName = 'material-components';
+  const componentPath = `src/app/core/${matFileName}.module.ts`;
+
+  return chain([addMaterialFramework(), addMaterialModule(), updateFiles()]);
+
+  function addMaterialFramework(): Rule {
+    return (tree: Tree, _context: SchematicContext) => {
+      return hasDependency(tree, '@angular/material')
+        ? noop()
+        : externalSchematic('@angular/material', 'material-shell', {
+            theme: 'custom',
+            typography: true,
+            animations: true,
+          });
+    };
+  }
+
+  function addMaterialModule(): Rule {
+    return (tree: Tree, _context: SchematicContext) => {
+      const text = tree.read(componentPath);
+
+      return text === null
+        ? externalSchematic('@schematics/angular', 'module', {
+            name: `/core/${matFileName}`,
+            skipTests: true,
+            flat: true,
+          })
+        : noop();
+    };
+  }
+
+  function updateFiles(): Rule {
+    return (tree: Tree, _context: SchematicContext) => {
+      
+      addImport(tree, componentPath, 'FormsModule', '@angular/forms');
+      
+      // Add Material Modules to Material Components module
+      matModuleList.forEach((mat) => {
+        addImport(tree, componentPath, mat.module, mat.path);
+
+        addExport(tree, componentPath, mat.module, null);
+      });
+
+      // Add Material Components Module to main App module
+      addImport(
+        tree,
+        'src/app/app.module.ts',
+        'MaterialComponentsModule',
+        './core/material-components.module'
+      );
+
+      return tree;
+    };
+  }
+}
